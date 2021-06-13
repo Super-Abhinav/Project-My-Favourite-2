65028a8c79d58039f41c56b0a60ebd03c6c444aa
Abhinav Anand <85018022+Super-Abhinav@users.noreply.github.com>
2021-06-13 14:37:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function Home() {
     window.open("https://super-abhinav.github.io/Project-My-Favourite-2/","_blank");
 }
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "<abhinav.ananddps2807@gmail.com>",
+	Password : "<9431200745>",
+	To : '<P-1673-16@dpspatna.com>',
+	From : "<abhinav.ananddps2807@gmail.com>",
+	Subject : "<email subject>",
+	Body : "<email body>",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
